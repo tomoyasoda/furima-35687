7d6b1b85322bbfb8b5f3faee93059e172009de09
tomoyasoda <sodatomoya1@gmail.com>
2021-06-09 12:23:36 +0900

    ユーザー管理機能修正５

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,16 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }           
-    validates :first_kana_name, format: {with: /\A[ァ-ヶー]+\z/, message: "全角カタカナを使用してください"}
-    validates :last_kana_name,  format: {with: /\A[ァ-ヶー]+\z/, message: "全角カタカナを使用してください"}
+    with_options format do
+      {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+      validates :first_name
+      validates :last_name
+    end           
+    with_options format do
+      {with: /\A[ァ-ヶー]+\z/, message: "全角カタカナを使用してください"}
+      validates :first_kana_name 
+      validates :last_kana_name
+    end
     validates :birthday
 
     validates :password, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i, message: "英数字文字6以上"}
