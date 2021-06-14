920fc65ae3df938348626536f039db334ffa6378
tomoyasoda <sodatomoya1@gmail.com>
2021-06-14 09:33:40 +0900

    出品機能修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
 
   end
 
   def new
     @item = Item.new
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
   end
 
   def create
