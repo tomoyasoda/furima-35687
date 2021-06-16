817ba1f2ce4c478ab9755d3c6ddb69dcfdb80682
tomoyasoda <sodatomoya1@gmail.com>
2021-06-16 18:00:27 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:show, :edit, :update, ]
+  before_action :contributor_confirmation, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
 
 
@@ -38,6 +38,11 @@ class ItemsController < ApplicationController
     end
   end
 
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
 
 
   private
