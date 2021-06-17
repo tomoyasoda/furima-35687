9be12db85816d8c2f38f9968530ab3f4748e1c78
tomoyasoda <sodatomoya1@gmail.com>
2021-06-17 16:04:05 +0900

    商品削除機能　修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :destroy]
   before_action :contributor_confirmation, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
 
